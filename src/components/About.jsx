4b8782b07626af8b2cc30bc28942ae424b06ecac
Xiomara Garcia <xiomg1570@gmail.com>
2022-07-26 15:16:18 -0500

    mail

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import foto from './assets/insta1.jpg'
-import Cv from './assets/XiomaraG-CV.pdf'
+import Cv from './assets/CV-XiomaraGarcia.pdf'
 export const About = () => {
   return (
     <div className='about'>
@@ -10,8 +10,12 @@ export const About = () => {
                 <p>
                 Soy junior Fronend Developer, al descubrir la programación quedé enamorada
                 de la comunidad, la creatividad y la posibilidad de impactar en la sociedad
+                <br />
+                En cuanto a mis intereses, me gusta capacitarme constantemente,
+                 retarme a mi misma, mejorar mis habilidades blandas, ayudar a las personas y disfrutar del proceso.
+                Actualmente me estoy capacitando como desarrolladora fullStack con node, mongoDB, express y mySql.
                 </p>
-                <p>Actualmente me ubico en Medellin-Colombia y trabajo en la empresa Gaby-Parrot</p>
+                <p>Actualmente me ubico en Medellin-Colombia</p>
             </div>
             <div className='des-foto'>
                 <img src={foto} alt='Foto'/>
@@ -27,6 +31,10 @@ export const About = () => {
             <p><i className="bi bi-lightning-fill"></i>Control de versiones con GiT. GitHub </p>
             <p><i className="bi bi-lightning-fill"></i> Redux</p>
             <p><i className="bi bi-lightning-fill"></i> Friebase</p>
+            <p><i className="bi bi-lightning-fill"></i> strapi</p>
+            <p><i className="bi bi-lightning-fill"></i> Figma</p>
+            <p><i className="bi bi-lightning-fill"></i> react-native</p>
+            <p><i className="bi bi-lightning-fill"></i> node.js</p>
         </div>
         <a href={Cv} download={`${Cv}`} target="_blank" rel="noreferrer"
         className='btn p-1'>Descargar Cv</a>
--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,20 +1,17 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
-import{ init } from '@emailjs/browser';
 import { useNavigate } from 'react-router-dom';
-init("3yBxmHrcYLNM1dc6T");
+import Alert from 'react-bootstrap/Alert';
 
 export const Contacto = () => {
-  const form = useRef(),
-   navigate = useNavigate()
-
+ const  navigate = useNavigate(),
+        [show, setShow] = useState(false);
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('gmail', 'template_uetd9fp', form.current, '3yBxmHrcYLNM1dc6T')
+    emailjs.sendForm('gmail', 'template_0xosako', e.target, '3yBxmHrcYLNM1dc6T')
       .then((result) => {
           console.log(result.text);
-          navigate('landing')
+          setShow(true)
       }, (error) => {
           console.log(error.text);
       });
@@ -23,16 +20,29 @@ export const Contacto = () => {
   return (
       <div className='contacto'>
            <h2>Si está interesado en trabajar conmigo en su proximo proyecto, no dude en ponerse en contacto</h2>
-    <form ref={form} onSubmit={sendEmail}
+           <Alert show={show} >
+              <Alert.Heading>Gracias por su mensaje!</Alert.Heading>
+              <p>
+                Me pondré en contacto en el trascurso de las proximas 24 horas
+              </p>
+              <hr />
+              <div className="d-flex justify-content-end">
+                <button className='btn p-1' onClick={() => {setShow(false); navigate('landing')}}>
+                  Entiendo!
+                </button>
+              </div>
+          </Alert>
+    <form  onSubmit={sendEmail}
     className="d-flex flex-column   gap-4 ">
        
       
       <input type="text" name="user_name" placeholder='nombre'/>
       
       <input type="email" name="user_email" placeholder='Correo' />
-      <textarea name="message" placeholder='Mensaje' />
+      <textarea name="user_message" placeholder='Mensaje' />
       <input type="submit" value="Send" className='btn'/>
     </form>
+    
     </div>
   );
 };
